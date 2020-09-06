d0a52ddd629cbb84761e72af0a91d464eff48262
Aarush Padki <67534760+Aarush504@users.noreply.github.com>
2020-09-06 22:13:33 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,11 @@
+class Pig extends BaseClass {
+  constructor(x){
+    super(x,1190,50,50,90);
+     /*var options = {
+       isStatic: true
+     }*/
+
+    this.image = loadImage("sprites/mango.png");
+  }
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# crispy-palm-tree
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
